33bcd8bd51a128f6893f44a7a8b8aae622cee88f
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2021-07-26 02:30:58 +0100

    keybind time

--- src/main/java/amymialee/visiblebarriers/client/VisibleBarriersClient.java
+++ src/main/java/amymialee/visiblebarriers/client/VisibleBarriersClient.java
@@ -4,15 +4,35 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class VisibleBarriersClient implements ClientModInitializer {
+    private static KeyBinding keyBinding;
+    public static boolean visible = false;
+
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.BARRIER, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.STRUCTURE_VOID, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.LIGHT, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.BARRIER, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.STRUCTURE_VOID, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.LIGHT, RenderLayer.getTranslucent());
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.visiblebarriers.bind",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_B,
+                "category.visiblebarriers.bind"
+        ));
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (keyBinding.wasPressed()) {
+                visible = !visible;
+                client.worldRenderer.reload();
+            }
+        });
     }
 }
--- src/main/java/amymialee/visiblebarriers/mixin/StructureVoidBlockMixin.java
+++ src/main/java/amymialee/visiblebarriers/mixin/StructureVoidBlockMixin.java
@@ -1,35 +1,20 @@
 package amymialee.visiblebarriers.mixin;
 
-import net.minecraft.block.*;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
-import net.minecraft.world.BlockView;
+import amymialee.visiblebarriers.client.VisibleBarriersClient;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.StructureVoidBlock;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(StructureVoidBlock.class)
-public class StructureVoidBlockMixin extends Block {
-    public StructureVoidBlockMixin(Settings settings) {
-        super(settings);
-    }
-
+public class StructureVoidBlockMixin {
     @Inject(method = "getRenderType", at = @At("HEAD"), cancellable = true)
     public void getRenderType(BlockState state, CallbackInfoReturnable<BlockRenderType> cir) {
-        cir.setReturnValue(BlockRenderType.MODEL);
-    }
-
-    @Unique
-    public boolean isSideInvisible(BlockState state, BlockState stateFrom, Direction direction) {
-        return stateFrom.isOf(this) || super.isSideInvisible(state, stateFrom, direction);
-    }
-
-    @Unique
-    public VoxelShape getCameraCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return VoxelShapes.empty();
+        if (VisibleBarriersClient.visible) {
+            cir.setReturnValue(BlockRenderType.MODEL);
+        }
     }
 }
