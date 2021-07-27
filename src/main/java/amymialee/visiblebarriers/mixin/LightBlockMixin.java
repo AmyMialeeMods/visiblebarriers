6d688a2a0c1bc496d2f3daaafc3ae41df10b929d
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2021-07-27 01:15:33 +0100

    light crash

--- src/main/java/amymialee/visiblebarriers/mixin/LightBlockMixin.java
+++ src/main/java/amymialee/visiblebarriers/mixin/LightBlockMixin.java
@@ -4,6 +4,11 @@ import amymialee.visiblebarriers.client.VisibleBarriersClient;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LightBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,6 +20,13 @@ public class LightBlockMixin {
     public void getRenderType(BlockState state, CallbackInfoReturnable<BlockRenderType> cir) {
         if (VisibleBarriersClient.visible) {
             cir.setReturnValue(BlockRenderType.MODEL);
+        }
+    }
+
+    @Inject(method = "getOutlineShape", at = @At("HEAD"), cancellable = true)
+    public void getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
+        if (VisibleBarriersClient.visible) {
+            cir.setReturnValue(VoxelShapes.fullCube());
         }
     }
 }
