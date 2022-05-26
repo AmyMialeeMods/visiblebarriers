7802275bf8f5d2fc417bfa641968503cd0dae514
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2022-05-26 06:40:16 +0100

    newinput

--- src/main/java/amymialee/visiblebarriers/VisibleBarriers.java
+++ src/main/java/amymialee/visiblebarriers/VisibleBarriers.java
@@ -10,7 +10,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.util.InputUtil;
@@ -22,14 +22,13 @@ import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class VisibleBarriers implements ClientModInitializer {
-    public final static String MODID = "visiblebarriers";
+    public final static String MOD_ID = "visiblebarriers";
     public static VisibleBarriersConfig config = null;
-    private static KeyBinding keyBinding;
 
     public static boolean visible = false;
     public static boolean visible_air = false;
 
-    public static final ItemGroup EXTRA_ITEMS = FabricItemGroupBuilder.create(new Identifier(MODID, "extra_items")).icon(() -> new ItemStack(Items.COMMAND_BLOCK))
+    public static final ItemGroup EXTRA_ITEMS = FabricItemGroupBuilder.create(new Identifier(MOD_ID, "extra_items")).icon(() -> new ItemStack(Items.COMMAND_BLOCK))
             .appendItems(stacks -> {
                 stacks.add(new ItemStack(Items.COMMAND_BLOCK));
                 stacks.add(new ItemStack(Items.CHAIN_COMMAND_BLOCK));
@@ -62,25 +61,6 @@ public class VisibleBarriers implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.CAVE_AIR, RenderLayer.getTranslucent());
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.VOID_AIR, RenderLayer.getTranslucent());
 
-        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.visiblebarriers.bind",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_B,
-                "category.visiblebarriers.bind"
-        ));
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (keyBinding.wasPressed()) {
-                visible = !visible;
-                if (Screen.hasShiftDown()) {
-                    if (visible) {
-                        visible_air = true;
-                    }
-                }
-                if (!visible) {
-                    visible_air = false;
-                }
-                client.worldRenderer.reload();
-            }
-        });
+        KeyBindingHelper.registerKeyBinding(VisibleControls.VISIBILITY);
     }
 }
--- src/main/java/amymialee/visiblebarriers/VisibleBarriersConfig.java
+++ src/main/java/amymialee/visiblebarriers/VisibleBarriersConfig.java
@@ -3,7 +3,7 @@ package amymialee.visiblebarriers;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 
-@Config(name = VisibleBarriers.MODID)
+@Config(name = VisibleBarriers.MOD_ID)
 public final class VisibleBarriersConfig implements ConfigData {
     /// Decides if you can save items in the hotbar tab by dragging them in.
     public boolean allowHotbarQuickSaving = false;
